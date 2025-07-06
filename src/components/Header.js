831e2b9529a99f46593f9484f5fb587f0c5b279e
venkatesh <venkatesh.sagard@gmail.com>
2025-07-06 16:58:52 +0530

    header changes done for staging

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
   return (
     <header className="hero">
-      <h1>Venkatesh Sagare</h1>
+      <h1>Venkatesh Sagar Staging</h1>
       <p>AWS Cloud & DevOps Enthusiast</p>
     </header>
   );
